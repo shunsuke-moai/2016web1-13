a5ac06ddc78b90a611104582207150ffcc33c834
shunsuke-moai <g1527035@st.t-kougei.ac.jp>
2016-07-04 17:31:52 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,19 +1,18 @@
 var separate_time=function(time){
-  var sec=time.getSeconds();
-  var min=time.getMinutes();
-  var hours=time.getHours();
-  var days=time.getDate();
-  var month=time.getMonth();
-  var year=time.getFullYear();
-  return[sec,min,hours,days,month,year];
+  var sec=Math.floor((time/1000)%60);
+  var min=Math.floor((time/1000/60)%60);
+  var hours=Math.floor((time/1000/60/60)%24);
+  var days=Math.floor(time/1000/60/60/24);
+  return[sec,min,hours,days];
 }
 
 var update=function(){
  var now=new Date();
- var counter=separate_time(now);
- document,getElementById('countdown').textContent=
- counter[5]+'年'+
- counter[4]+'月'+
+ var target=new Date(2020,7,24,0,0,0,0);
+ var diff=target.getTime()-now.getTime();
+ var counter=separate_time(diff);
+ document.getElementById('countdown').tectContent=
+ '東京オリンピックまであと'+
  counter[3]+'日'+
  counter[2]+'時'+
  counter[1]+'分'+
@@ -26,7 +25,7 @@ var refresh=function(){
 }
 update();
 
-document.getElementById('form').select.onchange=function2(){
+document.getElementById('form').select.onchange=function(){
   location.href=document.getElementById('form').select.value;
 }
 
