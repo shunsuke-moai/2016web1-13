f8e709590e8339ca16f0f3ad3b9cf80d108977c4
shunsuke-moai <g1527035@st.t-kougei.ac.jp>
2016-07-04 17:21:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,42 @@
+var separate_time=function(time){
+  var sec=time.getSeconds();
+  var min=time.getMinutes();
+  var hours=time.getHours();
+  var days=time.getDate();
+  var month=time.getMonth();
+  var year=time.getFullYear();
+  return[sec,min,hours,days,month,year];
+}
 
+var update=function(){
+ var now=new Date();
+ var counter=separate_time(now);
+ document,getElementById('countdown').textContent=
+ counter[5]+'年'+
+ counter[4]+'月'+
+ counter[3]+'日'+
+ counter[2]+'時'+
+ counter[1]+'分'+
+ counter[0]+'秒';
+ refresh();
+}
+
+var refresh=function(){
+  setTimeout(update,1000);
+}
+update();
+
+document.getElementById('form').select.onchange=function2(){
+  location.href=document.getElementById('form').select.value;
+}
+
+var last_date=getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie').textContent='前回訪れた時間'+last_date;
+}
+else{
+  document.getElementById('cookie').textContent='はじめまして';
+}
+
+var current_time=new Date();
+setCookie('lastDate',current_time.toString(),7);
